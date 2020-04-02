991a03288320f1d0cabff69e0df22c35b101a410
jfnavarro <jc.fernandez.navarro@gmail.comm>
2020-04-02 10:21:18 +0200

    Worked a bit on the variant calling part

--- Full_exome_pipeline.py
+++ Full_exome_pipeline.py
@@ -8,11 +8,6 @@ import time
 from re import sub
 
 
-# TODO finish this
-def translate_dna(seq):
-    return seq
-
-
 # All these programs must be in system or in PATH
 PICARD = 'java -Xmx32g picard.jar'
 SAMTOOLS = 'samtools'
@@ -40,11 +35,32 @@ LIBRARY = "Library"
 SOURCE = "Source"
 SAMPLE_NOTE = "Sample"
 RESECTION_DATE = "Na"
-RUN_DATA = "Na"
+RUN_DATE = "Na"
 SEQUENCER = "Na"
 KIT = "Na"
 NOTE = "Na"
 INDEX = "Na"
+
+FASTA_AA_DICT = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+}
+
+FASTA_cDNA = {'A':'A', 'G':'G', 'C':'C', 'T':'U'}
 
 def index_column_substring(your_list, substring):
     for i, s in enumerate(your_list):
@@ -52,6 +68,24 @@ def index_column_substring(your_list, substring):
             return i
     return -1
 
+def translate_dna(seq):
+    rna = ""
+    # Generate the RNA string
+    for i in seq:
+        # Replace all occurrences of T with U
+        if i == "T":
+            rna += "U"
+        else:
+            rna += i
+    return rna
+
+def translate_dna_to_protein(seq):
+    protein = ""
+    if len(seq) % 3 == 0:
+        for i in range(0, len(seq), 3):
+            codon = seq[i:i + 3]
+            protein += FASTA_AA_DICT[codon]
+    return protein
 
 def HLA_PRG(bamfile, sampleID, outfile):
     cpu_count = multiprocessing.cpu_count() - 1
