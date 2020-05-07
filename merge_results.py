80259751eb926dad520e1224bd5ffc4903cb55fc
jfnavarro <jc.fernandez.navarro@gmail.comm>
2020-05-07 11:30:01 +0200

    Fixed percentile column in merge

--- merge_results.py
+++ merge_results.py
@@ -189,12 +189,12 @@ def overlap_analysis(exome_variants, exome_epitopes, rna_variants, rna_fpkm):
             locus = columns[header_fpkm.index('locus')]
             fpkm_value = float(columns[header_fpkm.index('FPKM')])
             sample = columns[header_fpkm.index('SAMPLE_ID')]
-            if gene_id not in FPKM_dict:
-                FPKM_dict[gene_id] = {}
-            if sample not in FPKM_dict[gene_id]:
-                FPKM_dict[gene_id][sample] = {}
             #NOTE check for precision errors here
             if fpkm_value != 0:
+                if gene_id not in FPKM_dict:
+                    FPKM_dict[gene_id] = {}
+                if sample not in FPKM_dict[gene_id]:
+                    FPKM_dict[gene_id][sample] = {}
                 FPKM_dict[gene_id][sample]['locus'] = locus
                 FPKM_dict[gene_id][sample]['expression'] = fpkm_value
                 FPKM_dict_sample[sample].append(fpkm_value)
