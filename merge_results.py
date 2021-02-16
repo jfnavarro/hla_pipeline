565b5801a8c3e43c4738f45168736fb04297669d
jfnavarro <jc.fernandez.navarro@gmail.com>
2021-02-16 10:01:21 +0100

    Make the percentile computation strict

--- merge_results.py
+++ merge_results.py
@@ -109,7 +109,7 @@ def main(dna_variants,
                 mean = np.around(statistics.mean(counts), 3)
                 counts_stats[name] = mean
                 for gene, count in gene_counts.items():
-                    counts_stats_percentile[name][gene] = np.around(stats.percentileofscore(counts, count), 3)
+                    counts_stats_percentile[name][gene] = np.around(stats.percentileofscore(counts, count, kind='strict'), 3)
 
     print('Creating merged variants..')
     header_final = 'Variant key\tDBsnp ID\tGnomad MAF\tCosmic ID\tDNA samples (passing)\tNumber of DNA samples (passing)\t' \
